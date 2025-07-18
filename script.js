6b5ab6486a2c674890a861836f2b7ab16c3c4062
7Umair-Ahmed7 <7umairahmed7@gmail.com>
2025-07-19 00:24:53 +0500

    Login page completed

---
+++ script.js
@@ -0,0 +1,53 @@
+let passwordInput = document.getElementById("password")
+let emailInput = document.getElementById("email")
+let submitBtn = document.getElementById("submit")
+let errorEmail = document.getElementsByClassName("error-email")[0]
+let errorPassword = document.getElementsByClassName("error-password")[0]
+let form = document.getElementsByTagName("form")[0]
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (emailInput.value.length == 0) {
+        errorEmail.innerHTML = "email is required";
+    }
+    if (passwordInput.value.length == 0) {
+        errorPassword.innerHTML = "password is required"
+
+    }
+    if (passwordInput.value.length < 8) {
+        errorPassword = "password must have 8 characters"
+    }
+    if (passwordInput.value.length >= 8 && emailInput.value.length > 0 && emailInput.value.includes("@")) {
+        form.innerHTML = "Form successfully submitted"
+    }
+
+
+})
+emailInput.addEventListener("input", (e) => {
+    console.log(e.target.value)
+    if (emailInput.value.length == 0) {
+        errorEmail.innerHTML = "email is required";
+    }
+    else if (emailInput.value.includes("@") == false) {
+        errorEmail.innerHTML = "email is invalid";
+
+    }
+    else if (emailInput.value.length > 0 || emailInput.value.includes("@") == true) {
+
+        errorEmail.innerHTML = "";
+    }
+})
+
+
+passwordInput.addEventListener("input", () => {
+    if (passwordInput.value.length == 0) {
+        errorPassword.innerHTML = "password is required"
+
+    }
+    else if (passwordInput.value.length < 8) {
+        errorPassword.innerHTML = "password must have 8 characters"
+    }
+    else if (passwordInput.value.length >= 8) {
+        errorPassword.innerHTML = ''
+    }
+})
